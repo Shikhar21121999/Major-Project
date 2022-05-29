42d4784f0946753e8145daa0cd553c27f13e3cc5
shikharsun <shikhar21121999@gmail.com>
2022-05-29 21:10:21 +0530

    changes

--- src/server/server/signLanguageConverter/WordClassifier.py
+++ src/server/server/signLanguageConverter/WordClassifier.py
@@ -5,21 +5,22 @@ class WordClassifier:
     def __init__(self, statement):
         self.statement = statement
 
-    def getFName(self, fName):
-        return  "/assets/images/{}.jpg".format(fName)
+    def getFName(self, fName, ext):
+        return  "/assets/images/{}.{}".format(fName, ext)
 
     def fetchImageNames(self):
+        res = []
         # check if whole of the statement in wordBag
         if self.statement in WordBag.phrases:
-            return [self.getFName(self.statement)]
+            return [[self.getFName(self.statement, 'gif')]]
         
-        res = []
         # try forming statement using words and letters
         for word in self.statement.split(' '):
             if word in WordBag.words:
-                res.append(self.getFName(word))
+                res.append([self.getFName(word, 'gif')])
             else:
-                for letter in word:
-                    res.append(self.getFName(letter))
-        
+                temp = []
+                for letter in word.upper():
+                    temp.append(self.getFName(letter, 'jpg'))
+                res.append(temp)
         return res
