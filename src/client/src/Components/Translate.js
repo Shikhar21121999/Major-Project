726b295e55352cfa843c8f3882c336a44b026f7c
Udai Gupta <udaigupta19311@gmail.com>
2022-05-29 21:08:16 +0530

    Script added.

--- src/client/src/Components/Translate.js
+++ src/client/src/Components/Translate.js
@@ -69,7 +69,7 @@ function Translate() {
         try {
             setIsListening(false)
             setLoading(true)
-            // let res = axios.get(`/signLanguage/?statement${note}`);
+            // let res = axios.get(`/signLanguage/?statement${note.toLowerCase()}`);
             let imagesArr;
             // imagesArr = res.data.imageNames;
             imagesArr = [['https://res.cloudinary.com/maharaja-surajmal-institute-of-technology-msit-new-delhi/image/upload/v1653832511/b_to2q7c.jpg', 'https://res.cloudinary.com/maharaja-surajmal-institute-of-technology-msit-new-delhi/image/upload/v1653832411/a_ar3bt4.jpg'], ['https://res.cloudinary.com/maharaja-surajmal-institute-of-technology-msit-new-delhi/image/upload/v1653832548/address_e78aot.gif'], ['https://res.cloudinary.com/maharaja-surajmal-institute-of-technology-msit-new-delhi/image/upload/v1653832411/a_ar3bt4.jpg', 'https://res.cloudinary.com/maharaja-surajmal-institute-of-technology-msit-new-delhi/image/upload/v1653832511/b_to2q7c.jpg']]
